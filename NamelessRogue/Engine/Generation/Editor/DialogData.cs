46657e14eeefbfa9bd98b87144330c3c4847c12d
Alex Calu <alex_calu1@yahoo.com>
2025-01-31 22:25:39 +0200

    DialogData Create an xml file to store our dialog tree #9

---
+++ NamelessRogue/Engine/Generation/Editor/DialogData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NamelessRogue.Engine.Generation.Editor
+{
+    [XmlRoot]
+    public class DialogData
+    {
+        [XmlElement]
+        public string Id { get; set; }
+        [XmlElement]
+        public string Response { get; set; }
+        [XmlArray]
+        public DialogOption[] Options { get; set; }
+        
+        public DialogData() {}
+    }
+    public class DialogOption
+    {
+        [XmlElement]
+        public string Id { get; set; }
+        [XmlElement]
+        public string OptionText { get; set; }
+        [XmlElement]
+        public  DialogData DialogData{ get; set; }
+         
+
+    }
+}
