3749ded49b4fc4d6d142581af313087d932156e2
greyarmor <greyarmor@gmail.com>
2025-04-06 16:46:20 +0300

    messing up with buildings;

--- NamelessRogue/Content/Buildings/tileset2.tsx
+++ NamelessRogue/Content/Buildings/tileset2.tsx
@@ -51,6 +51,46 @@
    <property name="ObjectType" value="wall"/>
   </properties>
  </tile>
+ <tile id="10">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="ObjectType" value="door_brick"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="ObjectType" value="window_brick"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="ObjectType" value="door_open_brick"/>
+  </properties>
+ </tile>
  <tile id="20">
   <properties>
    <property name="ObjectType" value="wall"/>
@@ -101,6 +141,46 @@
    <property name="ObjectType" value="wall"/>
   </properties>
  </tile>
+ <tile id="30">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="ObjectType" value="door_brick"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="ObjectType" value="window_brick"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="ObjectType" value="door_open_brick"/>
+  </properties>
+ </tile>
  <tile id="40">
   <properties>
    <property name="ObjectType" value="wall"/>
@@ -149,6 +229,46 @@
  <tile id="49">
   <properties>
    <property name="ObjectType" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="ObjectType" value="wall_brick"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="ObjectType" value=""/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="ObjectType" value=""/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="ObjectType" value=""/>
   </properties>
  </tile>
  <tile id="60">
