79518a2a84b16598a6812409f34217dec2343846
greyarmor <greyarmor@gmail.com>
2025-12-05 16:15:15 +0200

    editor app progress;

---
+++ NamelessRogueDataEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NamelessRogueDataEditor.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [RelayCommand]
+        void SaveFile() { /* ... */ }
+
+        [RelayCommand]
+        void NewFile() { /* ... */ }
+
+        [RelayCommand]
+        void LoadFile() { /* ... */ }
+    }
+}
