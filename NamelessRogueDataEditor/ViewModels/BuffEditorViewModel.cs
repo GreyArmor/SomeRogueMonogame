ea3ffe76fc0e6858f50fba8e53ca2017c0247407
greyarmor <greyarmor@gmail.com>
2026-04-11 15:35:54 +0300

    editor progress;

--- NamelessRogueDataEditor/ViewModels/BuffEditorViewModel.cs
+++ NamelessRogueDataEditor/ViewModels/BuffEditorViewModel.cs
@@ -3,10 +3,12 @@ using CommunityToolkit.Mvvm.Input;
 using NamelessRogue.Engine.Generation.Editor;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 using Wpf.Ui.Controls;
 using Wpf.Ui.Extensions;
 
@@ -14,51 +16,135 @@ namespace NamelessRogueDataEditor.ViewModels
 {
     public partial class BuffEditorViewModel : ObservableObject
     {
-        [ObservableProperty]
-        private BuffTemplateData? buff;
+        string buffDirectory;
+        private BuffTemplateData buff;
 
+        List<string> buffFiles;
+        [ObservableProperty]
+        List<string> buffFileNames;
+
+        [ObservableProperty]
+        public string id;
+        [ObservableProperty]
+        public string name;
+        [ObservableProperty]
+        public string description;
+
+        [ObservableProperty]
+        public string iconPath;
+
+        [ObservableProperty]
+        public int duration;
+
+        [ObservableProperty]
+        public bool isAppliedImmediately;
+
+        [ObservableProperty]
+        public bool permanentModifier;
+
+        [ObservableProperty]
+        public bool isDamageOverTime;
+
+        [ObservableProperty]
+        public int healthModificator = 0;
+
+        [ObservableProperty]
+        public int energyModificator = 0;
+
+        [ObservableProperty]
+        public int armorModificator = 0;
+
+        [ObservableProperty]
+        public int resistanceModificator = 0;
+
+        [ObservableProperty]
+        public int damageModificator = 0;
+
+        [ObservableProperty]
+        bool canSave = false;
         public BuffEditorViewModel()
         {
-            buff = new BuffTemplateData
-            {
-                Name = "New Buff",
-                Description = "A new buff.",
-                Duration = 0,
-            };
+            buff = new BuffTemplateData();
+            buffDirectory = System.IO.Path.Combine(ContentDirectoryHelper.contentDirectoryPath, "GameObjects", "Buffs");
+            buffFiles = Directory.EnumerateFiles(buffDirectory, "*.nrbf").ToList();
+            buffFileNames = buffFiles.Select(x=>Path.GetFileName(x)).ToList();
+
+            PropertyChanged += (s, e) => { CanSave = !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Description) && !string.IsNullOrEmpty(IconPath) && !string.IsNullOrEmpty(Id); };
+        }
+
+        [RelayCommand]
+        public void Back()
+        {
+            App.Current.MainWindow.DataContext = new StartupWindowViewModel();
         }
 
         [RelayCommand]
         public void Save()
         {
+            buff.Id = id;
+            buff.Name = name;
+            buff.Description = description;
+            buff.IconPath = iconPath;
+            buff.Duration = duration;
+            buff.IsAppliedImmediately = isAppliedImmediately;
+            buff.PermanentModifier = permanentModifier;
+            buff.IsDamageOverTime = isDamageOverTime;
+            buff.HealthModificator = healthModificator;
+            buff.EnergyModificator = energyModificator;
+            buff.ArmorModificator = armorModificator;
+            buff.ResistanceModificator = resistanceModificator;
+            buff.DamageModificator = damageModificator;
+
+
+            var directory = System.IO.Path.Combine(ContentDirectoryHelper.contentDirectoryPath, "GameObjects", "Buffs");
+            var iconFileName = Path.GetFileName(iconPath);
+            if (!Directory.Exists(directory + "\\Icons\\"))
+            {
+                Directory.CreateDirectory(directory + "\\Icons\\");
+            }
+
+            //move icon to local directory
+            var newIconLocation = directory + "\\Icons\\" + iconFileName;
+
+            if (Path.IsPathFullyQualified(iconPath))
+            {
+                if (iconPath != newIconLocation)
+                {
+                    File.Copy(iconPath, newIconLocation, true);
+                }
+            }
+            buff.IconPath = Path.GetRelativePath(directory, directory + "\\Icons\\" + iconFileName);
+            IconPath = buff.IconPath;
             var serializer = new System.Xml.Serialization.XmlSerializer(typeof(BuffTemplateData));
-            var dir = System.IO.Path.Combine("..", "NamelessRogue", "Content", "GameObjects", "Buffs");
+            var dir = System.IO.Path.Combine(ContentDirectoryHelper.contentDirectoryPath, "GameObjects", "Buffs");
             System.IO.Directory.CreateDirectory(dir);
-            var path = System.IO.Path.Combine(dir, $"{Buff?.Name ?? "Buff"}.xml");
+            var path = System.IO.Path.Combine(dir, $"{buff?.Name ?? "Buff"}.nrbf");
             using var stream = System.IO.File.Create(path);
-            serializer.Serialize(stream, Buff);
+            serializer.Serialize(stream, buff);
             System.Windows.MessageBox.Show($"Saved to {path}");
         }
 
         [RelayCommand]
         public void SelectIcon()
         {
-            string file = "";
-            // Displays an OpenFileDialog so the user can select a file.  
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "Files|*.txt;*.out";
-            openFileDialog1.Title = "Select a File";
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            var dir = ContentDirectoryHelper.contentDirectoryPath;
+            System.IO.Directory.CreateDirectory(dir);
+            fileDialog.InitialDirectory = dir;
+            fileDialog.Filter = "Files|*.png;*.jpg";
+            fileDialog.Title = "Select an icon";
 
-            // Show the Dialog.  
-            // If the user clicked OK in the dialog and  
-            // a file was selected, open it.  
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            if (fileDialog.ShowDialog() == DialogResult.OK)
             {
-                file = openFileDialog1.FileName;
-                //file = openFileDialog1.OpenFile().ToString();
-                //openFileDialog1.Dispose();
+                if (File.Exists(fileDialog.FileName))
+                {
+                    IconPath = fileDialog.FileName;
+                }
             }
-            openFileDialog1 = null;
-            Console.WriteLine("File path is: " + file);
+          
+            fileDialog = null;
         }
+
+      
     }
 }
---
+++ NamelessRogueDataEditor/ViewModels/ContentDirectoryHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamelessRogueDataEditor.ViewModels
+{
+    public static class ContentDirectoryHelper
+    {
+        public static string contentDirectoryPath;
+
+        static ContentDirectoryHelper()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+#if DEBUG
+            string projectDirectory = Directory.GetParent(Directory.GetParent(workingDirectory).Parent.Parent.FullName).FullName + "\\NamelessRogue";
+            contentDirectoryPath = projectDirectory + "\\Content\\";
+#else
+            contentDirectoryPath = workingDirectory + "\\Content\\";
+#endif
+        }
+    }
+}
